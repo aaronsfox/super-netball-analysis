6d958482726f8a4cf9ea458301a5fb1712f6aa20
Aaron Fox <48617376+aaronsfox@users.noreply.github.com>
2020-09-08 19:30:32 +1000

    Identified solution to image cropping code.

--- Data/SuperNetball2020/Images/imageCropper.py
+++ Data/SuperNetball2020/Images/imageCropper.py
@@ -10,49 +10,21 @@ Created on Mon Sep  7 22:10:29 2020
     
 """
 
-from PIL import Image, ImageOps
-
-mask = Image.open('mask.png').convert('L')
-im = Image.open('A.Brazill.png')
-
-output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
-output.putalpha(mask)
-
-output.save('output.png')
-
-
-# %%
-
-from PIL import Image, ImageOps, ImageDraw
-
-size = (200, 200)
-mask = Image.new('L', size, 0)
-draw = ImageDraw.Draw(mask) 
-draw.ellipse((0, 0) + size, fill=0)
-
-
-im = Image.open('A.Brazill.png')
-
-
-output = ImageOps.fit(im, mask.size, centering=(0.5, 0.5))
-output.putalpha(mask)
-
-output.save('output.png')
-
-# %%
-
 from PIL import Image, ImageChops, ImageDraw
 
-####### THIS IS PROBABLY THE ONE, ALTHOUGH UNSURE WHETHER THE
-####### CIRCLE IS SYMETRICAL
+##### CODE WORKS! WITH MANUAL ASPECTS OF COURSE!!!!
+##### NEED TO ADJUST AND USE WITHIN LOOP FOR ALL
+
+##### PERHAPS STORE A CROPPED VERSION OF FILES (i.e. different name)...
 
 ###### Can't get size right for circle...
-
+#w = 440, h = 248
 im = Image.open('A.Brazill.png')
-bigsize = (im.size[0], im.size[0])
+bigsize = (im.size[0], im.size[1])
 mask = Image.new('L', bigsize, 0)
-ImageDraw.Draw(mask).ellipse((0, 0) + bigsize, fill=255)
-mask = mask.resize(im.size, Image.ANTIALIAS)
-# mask = ImageChops.darker(mask, im.split()[-1])
+# ImageDraw.Draw(mask).ellipse((0, 0) + bigsize, fill=255)
+ImageDraw.Draw(mask).ellipse([(220-124,0),(220+124,248)], fill=255)
+# mask = mask.resize(im.size, Image.ANTIALIAS)
+mask = ImageChops.darker(mask, im.split()[-1])
 im.putalpha(mask)
 im.save('cropped.png')
